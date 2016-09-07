7f705e27242f18bc7e2126980d8560d26c07adb3
Konstantin <e@logvik.com>
2016-09-07 11:21:40 +0300

    Update install_binbuild_tools.sh

--- binbuild/scripts/install_binbuild_tools.sh
+++ binbuild/scripts/install_binbuild_tools.sh
@@ -1,6 +1,8 @@
 set -e
+apt-get update -y
 # for npm module re-building
-apt-get -y install build-essential libssl-dev git python
+apt-get install -y curl bzip2 build-essential python git imagemagick libmagickcore-dev libmagickwand-dev
+
 npm install -g node-gyp
 # pre-install node source code for faster building
 node-gyp install ${NODE_VERSION}
